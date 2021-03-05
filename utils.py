368f49969249f0a0454308cfd67f45cc35b8aa36
bakerwm <wangmcas@gmail.com>
2021-03-05 10:05:43 +0800

    format argparse

--- utils.py
+++ utils.py
@@ -25,44 +25,42 @@ def get_args():
     """
     parser = argparse.ArgumentParser(description='piRNA pipe')
     parser.add_argument('-i', '--fq', required=True,
-        help='fastq file') 
+                        help='fastq file') 
     parser.add_argument('-o', '--outdir', required=True,
-        help='directory to save results')
-    parser.add_argument('-w', '--workflow', type=int, default=1,
-        help='workflow, 1:4, 1:TE->piRC->Genome; 2:TE->Genome; \
-        3:piRC->TE->Genome; 4:piRC->Genome;  default: [1]')
-    # parser.add_argument('-c', '--collapse', action='store_true',
-    #     help='collapse fastq reads')
-    parser.add_argument('-t', '--trimmed', action='store_true',
-        help='Input file was clean data, no need to trim adapters')
-    parser.add_argument('--subject', default=None,
-        help='The target file for overlap, bowtie_index, or fastq file')
-    parser.add_argument('--subject-list', nargs='+', dest='subject_list',
-        help='list of subject files, see --subject, default: []')
-    parser.add_argument('--ov-type', dest='ov_type', type=int, default=1,
-        help='overllap type, 1=perfect match, 2=allow mismatch, default: [1]')
-    parser.add_argument('-p', '--threads', type=int, default=4,
-        help='Number of threads to run')
-    parser.add_argument('-j', '--parallel-jobs', type=int, default=1,
-        dest='parallel_jobs',
-        help='number of jobs to run in parallel')
+                        help='directory to save results')
     parser.add_argument('-g', '--genome', default='dm6',
-        help='reference genome, default: [dm6]')
+                        help='reference genome, default: [dm6]')
+    parser.add_argument('-t', '--trimmed', action='store_true',
+                        help='No need to trim adapters')
+    parser.add_argument('-s', '--subject-list', nargs='+', dest='subject_list',
+                        defult=[],
+                        help='list of fastq files, for overlap, default: []')
+    parser.add_argument('--ov-type', dest='ov_type', type=int, default=1,
+                        help='overllap type, 1=perfect match, \
+                        2=allow mismatch, default: [1]')
+    parser.add_argument('-w', '--workflow', type=int, default=1,
+                        help='workflow, 1:4, 1:TE->piRC->Genome; \
+                        2:TE->Genome; 3:piRC->TE->Genome; \
+                        4:piRC->Genome;  default: [1]')
+    parser.add_argument('-p', '--threads', type=int, default=4,
+                        help='Number of threads to run')
+    parser.add_argument('-j', '--parallel-jobs', type=int, default=1,
+                        dest='parallel_jobs',
+                        help='number of jobs to run in parallel')
     parser.add_argument('-ir', '--index-rRNA', default='smRNA',
-        help='bowtie_index for rRNA, default: ')
+                        help='bowtie_index for rRNA, default: ')
     parser.add_argument('-im', '--index-miRNA', default='hairpin',
-        help='bowtie_index for miRNA (hairpin), default:')
+                        help='bowtie_index for miRNA (hairpin), default:')
     parser.add_argument('-ite', '--index-te', default='transposon',
-        help='bowtie_index for te (TE consensus), default:') 
+                        help='bowtie_index for te (TE consensus), default:') 
     parser.add_argument('-ip', '--index-piRNAcluster', default='piRNAcluster',
-        help='bowtie_index for piRNA cluster, default:') 
+                        help='bowtie_index for piRNA cluster, default:') 
     parser.add_argument('-ig', '--index-genome', default='genome',
-        help='bowtie_index for genome, default:') 
+                        help='bowtie_index for genome, default:') 
     parser.add_argument('-te', '--te-fa', default='te.fa',
-        help='TE sequence in fasta format') 
+                        help='TE sequence in fasta format') 
     parser.add_argument('-pi', '--piRNAcluster-fa', default='piRNAcluster.fa',
-        help='piRNA cluster sequence in fasta format')
-
+                        help='piRNA cluster sequence in fasta format')
     return parser.parse_args()
 
 
@@ -91,6 +89,7 @@ class PipeConfig(object):
             'trimmed': True,
             'force_overlap': False,
             'subject': None,
+            'subject_list': None,
             'ov_type': 1,
             'genome_path': '~/data/genome'
         }
@@ -104,8 +103,8 @@ class PipeConfig(object):
                 type(self.fq).__name))
         # smp_name
         if not isinstance(self.smp_name, str):
-            self.smp_name = fq_name(self.fq, pe_fix=True)
-        self.fq_name = self.smp_name + '.fq.gz' # force fastq output
+            self.smp_name = fq_name(self.fq, pe_fix=False)
+        self.fx_name = self.smp_name + '.fastq.gz' # force fastq output
         # outdir
         if not isinstance(self.outdir, str):
             self.outdir = str(pathlib.Path.cwd())
@@ -133,7 +132,8 @@ class PipeConfig(object):
             '{:>15s}: {}'.format('outdir', self.outdir),
             '{:>15s}: {}'.format('smp_name', self.smp_name),
             '{:>15s}: {}'.format('genome', self.genome),
-            '{:>15s}: {}'.format('subject', self.subject)
+            '{:>15s}: {}'.format('subject', self.subject),
+            '{:>15s}: {}'.format('overlap', 'yes' if self.force_overlap else 'no'),
         ])
         print('-'*80)
         print(msg)
@@ -147,27 +147,27 @@ class PipeConfig(object):
         overlap
         """        
         # subject, fastq for bowtie index
-        if isinstance(self.subject, str):
-            if file_exists(self.subject):
-                print("!AAAA-1")
-                pass
-            elif file_exists(self.subject+'.1.ebwt'):
-                print("!AAAA-2")
-                pass
-            else:
-                print("!AAAA-3")
+        if self.subject is None:
+            pass
+        elif isinstance(self.subject, str):
+            if not file_exists(self.subject):
                 self.subject = None
+        else:
+            raise ValueError('subject, expect str, got {}'.format(
+                type(self.subject).__name__))
+        # subject_list, from args
+        if self.subject_list is None:
+            self.subject_list = []
+        if isinstance(self.subject_list, str):
+            self.subject_list = [self.subject_list]
+        if not isinstance(self.subject_list, list):
+            self.subject_list = [] # force
+        self.subject_list = [i for i in self.subject_list if file_exists(i)]            
         # overlap dir
         if self.force_overlap and isinstance(self.subject, str):
-            if file_exists(self.subject+'.1.ebwt'):
-                subject_suffix = os.path.basename(self.subject)
-                self.ov_type = 2 # force to bowtie align mode
-            else:
-                subject_suffix = fq_name(self.subject)
-            self.prj_dir = os.path.join(
-                self.prj_dir, 
-                'overlap.{}.{}'.format(self.ov_type, subject_suffix)
-            )
+            pass
+            # s_name = fq_name(self.subject)
+            # self.prj_dir = os.path.join(self.prj_dir, 'overlap', s_name) #update
         else:
             self.subject = None # force not subject
         
@@ -235,6 +235,13 @@ class PipeConfig(object):
         ...
         """
         args_files = {
-            'config_toml': self.config_dir + '/config.toml'
+            'config_toml': self.config_dir + '/config.toml',
+            'fq_raw': os.path.join(self.raw_dir, self.fx_name),
+            'fq_clean': os.path.join(self.clean_dir, self.fx_name),
+            'fq_collapse': os.path.join(self.collapse_dir, self.fx_name),
+            'fq_overlap': os.path.join(self.overlap_dir, self.fx_name),
+            'fq_size_in': os.path.join(self.size_dir, self.fx_name),
+            'fq_size_ex': os.path.join(self.size_ex_dir, self.fx_name),
+            'fq_unal': os.path.join(self.unmap_dir, self.fx_name),
         }
         self = update_obj(self, args_files, force=True)
